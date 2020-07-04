c140626d8ea8f1826033279b907a7664836e6388
Nathalie Tate <nrt@nathalietate.xyz>
2020-07-04 16:16:45 -0400

    Updates

--- lib/nathalietate/xyz.pm
+++ lib/nathalietate/xyz.pm
@@ -7,7 +7,11 @@ our $VERSION = '0.1';
 
 get '/' => sub 
 {
-  template 'index' => { 'title' => 'Nathalie Tate' };
+  template 'index' => 
+  { 
+    'title' => 'Nathalie Tate',
+    'copy' => getCopy()
+  };
 };
 
 get '/license' => sub 
@@ -29,5 +33,19 @@ get '/writing' => sub
 {
   template 'writing' => { 'title' => 'Writing' }; 
 };
+
+sub getCopy
+{
+  my @phrases = 
+  (
+    "Strange but not a stranger",
+    "Decay exists as an extant form of life",
+    "Beyond the Flesh and the Real",
+    "You cannot kill me in a way that matters"
+  );
+
+
+  return $phrases[ rand @phrases ];
+}
 
 true;
